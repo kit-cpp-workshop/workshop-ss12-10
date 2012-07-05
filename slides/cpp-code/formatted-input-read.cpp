094e1bd5cb0c2530095f0ac612614a2e9ddf8eac
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-07-05 23:15:15 +0200

    Better example for read line

--- slides/cpp-code/formatted-input-read.cpp
+++ slides/cpp-code/formatted-input-read.cpp
@@ -31,45 +31,67 @@ while(true)
 }
 /*$ read_line $*/
 template < class C, class CT >
-basic_istream < C, CT >& ws_noLF( basic_istream < C, CT >& s )
-{
-	std::locale const& loc = s.getloc();
-	for( C c = s.peek();						// read but not consume next character
-	        s									// anything went wrong?
-		 && std::isspace(c, loc)				// c must be a white space
-		 && c != s.widen('\n');					// c must not be '\n'
-	     c = s.peek() )							// read but not consume next character
-	{ s.get(); }								// consume/discard next character
-	
-	return s;
+void input( basic_istream < C, CT >& in ) {
+	CT::int_type const LF = CT::to_int_type( cFacet.widen('\n') );	// get LF character
+	in >> skipws;												// (is by default)
+	vector < int > v;
+	while(in) {													// read until an extraction failed
+		int i;
+		while( in >> i ) {										// read an int
+			v.push_back(i);										// use that int read successfully
+			in >> ws_noLF;										// skip all white spaces but '\n'
+			if( CT::eq_int_type(LF, in.peek()) ) { break; }		// break loop on line end
+		}
+		/* use the vector v, watch out v.size() could be 0 */
+	}
 }
-/*$ locale_end $*/
-/*$ facet $*/
+/*$ iomanip_begin $*/
 template < class C, class CT >
 basic_istream < C, CT >& ws_noLF( basic_istream < C, CT >& s )
-{
-	std::ctype < C > const& fac = std::use_facet < std::ctype < C > > (loc);
-	for( C c = s.peek();						// read but not consume next character
-	        s									// anything went wrong?
-		 && fac.is(std::ctype_base::space, c)	// c must be a white space
-		 && c != fac.widen('\n');				// c must not be '\n'
-	     c = s.peek() )							// read but not consume next character
-	{ s.get(); }								// consume/discard next character
+/*$ iomanip_impl $*/{
+	locale const& loc = s.getloc();
+	ctype < C > const& cFacet = use_facet < ctype < C > > (loc);
+	for( CT::int_type c = s.peek();							// read but not consume next character
+	        s												// anything went wrong?
+		 && cFacet.is(cFacet.space, CT::to_char_type(c))	// c must be a white space
+		 && !CT::eq_int_type(LF, c);						// c must not be '\n'
+	     c = s.peek() )										// read but not consume next character
+	{ s.get(); }											// consume/discard next character
 	
 	return s;
 }
-/*$ facet_end $*/
-using Vec = vector < int >;
-Vec v;
-while(cin) {								// read until EOF or failure
-	int i;
-	while( cin >> i ) {						// read an int
-		v.push_back(i);						// use that int read successfully
+/*$ read_line_end $*/
 
-		cin >> ws_noLF;						// skip all white spaces but '\n'
-		if( cin.widen('\n') == cin.peek() )	// if line ends now
-		{ break; }
+/*$ locale_magin_begin $*/
+/*$locale_magic$*/template < typename C >
+/*$locale_magic$*/class ctype_noLF : public ctype<C> {
+/*$locale_magic$*/public:
+/*$locale_magic$*/	ctype_noLF(ctype const& p) : ctype(m_table) {
+/*$locale_magic$*/		copy(p.table(), p.table()+ctype<C>::table_size, m_table);
+/*$locale_magic$*/		m_table[(unsigned char)'\n'] = ctype_base::cntrl;
+/*$locale_magic$*/	}
+/*$locale_magic$*/protected:
+/*$locale_magic$*/	ctype_base::mask m_table[ctype<C>::table_size];
+/*$locale_magic$*/};
+
+template < class C, class CT >
+void input( basic_istream < C, CT >& in ) {
+	/*$locale_magic$*/locale oldLoc = in.getloc();
+	/*$locale_magic$*/ctype_noLF<C>* pMyFac = new ctype_noLF<C>( use_facet< ctype<C> >(oldLoc) );
+	/*$locale_magic$*/in.imbue( locale{oldLoc, pMyFac} );
+
+	typename CT::int_type const LF = CT::to_int_type( pMyFac->widen('\n') );	// get LF character
+	in >> skipws;												// (is by default)
+	vector < int > v;
+	while(in) {													// read until an extraction failed
+		int i;
+		while( in >> i ) {										// read an int
+			v.push_back(i);										// use that int read successfully
+			if( CT::eq_int_type(LF, in.peek()) )				// if line ends now ('\n' follows)
+			{ in.get(); break; }								// discard '\n' and break loop
+		}
+		/* use the vector v, watch out v.size() could be 0 */
 	}
-	//- next line
-	break;						// normally: do something useful instead; here: parse only one line
+	in.imbue(oldLoc);
 }
+/*$ locale_magin_end $*/
